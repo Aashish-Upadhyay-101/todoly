c8bed078a1986bfa7e8c0389b199669be149570f
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-11-27 01:25:08 +0545

    feat: listing boards

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,15 +1,43 @@
 import FormPopover from "@/components/form/form-popover";
 import Hint from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import { HelpCircle, User2 } from "lucide-react";
-import React from "react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function BoardList() {
+export default async function BoardList() {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect("/select-org");
+  }
+
+  const boards = await db.board.findMany({
+    where: {
+      orgId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="space-y-4">
       <div className="flex items-center font-semibold text-neutral-700 text-lg">
         <User2 className="h-6 w-6 mr-2" /> Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-col-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            href={`/board/${board.id}`}
+            className="aspect-video group relative bg-no-repeat bg-center bg-cover rounded-sm h-full w-full p-2 overflow-hidden bg-sky-700"
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}>
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="font-semibold text-white relative">{board.title}</p>
+          </Link>
+        ))}
         <FormPopover side="right" sideOffset={10}>
           <div
             role="button"
@@ -28,3 +56,16 @@ export default function BoardList() {
     </div>
   );
 }
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-col-4 gap-4">
+      <Skeleton className="rounded-sm h-full w-full aspect-video" />
+      <Skeleton className="rounded-sm h-full w-full aspect-video" />
+      <Skeleton className="rounded-sm h-full w-full aspect-video" />
+      <Skeleton className="rounded-sm h-full w-full aspect-video" />
+      <Skeleton className="rounded-sm h-full w-full aspect-video" />
+      <Skeleton className="rounded-sm h-full w-full aspect-video" />
+    </div>
+  );
+};

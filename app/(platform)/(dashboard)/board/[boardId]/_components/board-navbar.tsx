db771f2c4aac04efbe48847f355f0b140190963c
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-11-29 22:09:06 +0545

    feat: delete board

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,5 +1,6 @@
 import { Board } from "@prisma/client";
 import BoardTitleForm from "./board-title-form";
+import BoardOptions from "./board-options";
 
 interface BoardNavbarProps {
   board: Board;
@@ -9,7 +10,9 @@ export async function BoardNavbar({ board }: BoardNavbarProps) {
   return (
     <div className="w-full h-14 z-[40] fixed top-14 text-white px-6 flex items-center gap-x-4 bg-black/50">
       <BoardTitleForm board={board} />
-      <div className="ml-auto">{/* delete board option */}</div>
+      <div className="ml-auto">
+        <BoardOptions id={board.id} />
+      </div>
     </div>
   );
 }
